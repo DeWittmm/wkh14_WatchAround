e057296fb222e269bcc6c2b8cd1b52aa12e8dcf5
Quinton Petty <qlpetty@gmail.com>
2014-11-23 14:29:02 -0800

    Added janky tableview to mainview

--- WatchBump/WatchBump/ViewController.swift
+++ WatchBump/WatchBump/ViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+    
+    override init() {
+        super.init()
+        //table.registerClass(WBSettingCell.self, forCellReuseIdentifier: "Setting")
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        //fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.rowHeight = 44;
+        table.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +34,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:WBSettingCell = tableView.dequeueReusableCellWithIdentifier("Setting") as WBSettingCell
+        cell.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        //cell.textLabel?.text = "somehting"
 
+        return cell
+    }
 }
 
