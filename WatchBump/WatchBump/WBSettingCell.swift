df7e1c206d063d991f3d76ad13273838d7d2a550
Quinton Petty <qlpetty@gmail.com>
2014-11-23 16:37:54 -0800

    Added menu

--- WatchBump/WatchBump/WBSettingCell.swift
+++ WatchBump/WatchBump/WBSettingCell.swift
@@ -11,4 +11,13 @@ import UIKit
 
 class WBSettingCell: UITableViewCell {
     @IBOutlet weak var toggle: UISwitch!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var imageOfTitle: UIImageView!
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //imageView?.frame = CGRectMake(2.0, 2.0, 32.0, 32.0);
+    }
+
 }
